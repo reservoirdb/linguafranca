1ca45e74e7df9557f63422e74dac7ab5db936929
Ruan Pearce-Authers <ruanpa@outlook.com>
2021-02-15 19:14:44 +0000

    Fix table ref schema

--- linguafranca/types_table.py
+++ linguafranca/types_table.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass
 from enum import Enum
 
+from .types_schema import *
+
 @dataclass(frozen = True)
 class TableRef:
-	schema: str
+	schema: SchemaRef
 	name: str
 
 @dataclass
